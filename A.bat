97eb2c9caca6f8973f3be05ba68c8a9e6e9d98a1
tech1savvy <amankumar010604@gmail.com>
2025-05-05 23:44:15 +0530

    added prep files

---
+++ A.bat
@@ -0,0 +1,4 @@
+REM filepath: /home/tech1savvy/Devbook/lpu-cse-devops-etp/A.bat
+@echo off
+echo Running Script A
+pause
---
+++ B.bat
@@ -0,0 +1,4 @@
+REM filepath: /home/tech1savvy/Devbook/lpu-cse-devops-etp/B.bat
+@echo off
+echo Running Script B
+pause
